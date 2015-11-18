4b7c717c9f51a84e32d73655516743cef3e8311b
J.D.Song <juude.song@gmail.com>
2015-11-19 00:21:34 +0800

    play circle`

--- circle/sketch.js
+++ circle/sketch.js
@@ -9,6 +9,37 @@ var xspeed = 2.5;
 var yspeed = 2;
 var canvasWidth = 300;
 var canvasHeight = 300;
+var radius = 100;
+var angle = 0;
+var clock = true;
+var DEBUG = false;
+var gameOver = false;
+var circleColors = [
+    color(230, 143, 143),
+    color(125, 240, 125),
+    color(0, 0, 240),
+    color(250, 0, 250),
+    color(255, 222, 8)];
+
+var colorSize = circleColors.length;
+
+var randomInt = function(low, high) {
+    var result =  floor(random(low, high + 1));
+    if(result === high+1) {
+        result = low;
+    }
+    return result;
+};
+
+var randomExclude = function(low, high, exclude) {
+    var result = randomInt(low, high-1);
+    if(result >= exclude) {
+        result = exclude + 1;
+    }
+    return result;
+};
+var hitted = false;
+var lineColorIndex = randomExclude(0, colorSize -1, 1);
 
 function setup() {
   createCanvas(canvasWidth, canvasHeight);
@@ -16,7 +47,7 @@ function setup() {
 
 function draw() {
   rect(0, 0, canvasWidth, canvasHeight);
-  background(51);
+  //background(51);
 
   // Add the current speed to the position.
   x = x + xspeed;
@@ -33,7 +64,43 @@ function draw() {
   stroke(0);
   strokeWeight(2);
   fill(255, 0, 0);
-  ellipse(x, y, 48, 48);
+  ellipse(x, y, 48, 4);
+  //real circle
+  noStroke();
+  fill(36, 1, 1);
+
+  var centerX = canvasWidth / 2;
+  var centerY = canvasHeight / 2;
+
+  //ellipse(centerX, centerY, radius * 2, radius * 2);
+  var range = 360 / circleColors.length;
+  //if(DEBUG) {
+  //println(circleColors.length + " range " + range);
+  //}
+  var angleBegin = 0;
+  strokeWeight(2);
+  for(var i = 0; i < circleColors.length; i++) {
+      stroke(circleColors[i]);
+      arc(centerX, centerY, radius * 2, radius * 2, angleBegin, angleBegin + range);
+      angleBegin = angleBegin + range;
+  }
+  angle += (clock ?  1 : -1) * 3;
+  if(angle > 360) {
+      angle -= 360;
+  }
+  if(angle < -360) {
+      angle += 360;
+  }
+  
+  var colorIndex = floor((angle/range)) % circleColors.length;
+  if(DEBUG) {
+      println("range : " + range + " angle: " + angle + "colorIndex: " + colorIndex);
+  }
+  var currentColor = circleColors[colorIndex];
+  stroke(circleColors[lineColorIndex]);
+  line(centerX, centerY, centerX + radius * cos(angle), centerY + radius * sin(angle));
+  var hit = colorIndex === lineColorIndex;
+
 
 };
 
